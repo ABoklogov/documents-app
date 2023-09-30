d3e846df087020b319b994da175bb9a1963fbee7
Alexey <74319867+ABoklogov@users.noreply.github.com>
2023-09-30 11:01:55 +0300

    does add component MyDashboard

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyDashboard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyDashboard from "./components/MyDashboard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyDashboard,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +18,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
